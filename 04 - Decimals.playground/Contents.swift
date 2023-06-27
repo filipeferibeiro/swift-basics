49d372bd7872ef0a3ddd173de45c2fc5eb5f3456
Filipe Fernandes <filipeferibeiro@outlook.com>
2023-06-26 23:02:35 -0300

    Day 1

--- 04 - Decimals.playground/Contents.swift
+++ 04 - Decimals.playground/Contents.swift
@@ -1,3 +1,23 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+// MARK: - Imprecisao dos decimais
+let number = 0.1 + 0.2 // Double
+print(number)
+
+// MARK: - Operacoes com decimais
+let a = 1 // Int
+let b = 2.0 // Double
+let c = Double(a) + b // Swift nao deixa operacoes de soma com tipos diferentes
+
+// MARK: - Declarando decimais
+let double1 = 3.1
+let double2 = 3131.3131
+let double3 = 3.0
+let int1 = 3
+
+// MARK: - Outras operacoes
+var rating = 5.0
+rating *= 2
+print(rating)
+
+// CGFloat = Double
