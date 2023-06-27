867c6863343efb85198d87d8a68fceedae13dcb1
Filipe Fernandes <filipeferibeiro@outlook.com>
2023-06-27 13:43:25 -0300

    Day 2

---
+++ 06 - Join Strings.playground/Contents.swift
@@ -0,0 +1,21 @@
+import Cocoa
+
+let firstPart = "Hello, "
+let secontPart = "world!"
+let greeting = firstPart + secontPart
+
+let people = "Haters"
+let action = "hate"
+let lyric = people + " gonna " + action
+
+let luggageCode = "1" + "2" + "3" + "4" + "5" // Nao faz de uma vez so, faz soma por soma
+
+let name = "Taylor"
+let age = 26
+let message = "Hello, my name is \(name) and I'm \(age) years old." // Mais eficiente
+print(message)
+
+let number = 11
+let missionMessage = "Apollo \(number) landed on the moon."
+
+print("5 x 5 is \(5 * 5)")
---
+++ Checkpoint 1.playground/Contents.swift
@@ -0,0 +1,6 @@
+import Cocoa
+
+let temperatureC = 25.0
+let temperatureF = (temperatureC * 9)/5 + 32
+
+print("The temperature is \(temperatureC)°C/\(temperatureF)°F")
