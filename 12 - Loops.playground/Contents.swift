7f6e2c2bc9716a064bdecaf375cda7d334ec1fce
Filipe Fernandes <filipeferibeiro@outlook.com>
2023-06-29 11:52:37 -0300

    Day 5 and 6

---
+++ 12 - Loops.playground/Contents.swift
@@ -0,0 +1,35 @@
+import Cocoa
+
+// MARK: - FOR
+let platforms = ["iOS", "macOS", "tvOS", "watchOS"]
+
+for os in platforms {
+    print("Swift work grat on \(os).")
+}
+
+for i in 1...12 {
+    print("5 x \(i) is \(5 * i)")
+}
+
+for i in 1...12 {
+    print ("The \(i) times table")
+    for j in 1...12 {
+        print("   \(j) x \(i) is \(j * i)")
+    }
+    print()
+}
+
+for i in 1...5 {
+    print("Counting from 1 through 5: \(i)")
+}
+
+for i in 1..<5 {
+    print("Counting from 1 up to 5: \(i)")
+}
+
+var lyric = "Haters gonna"
+for _ in 1...5 {
+    lyric += " hate"
+}
+
+print(lyric)
